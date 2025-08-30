847700328d57a140df076817c823ab64e4a858ed
nisio <nisios@outlook.com>
2025-08-30 23:27:53 +0100

    Hammer Game!!!

--- scripts/hammerGame/game.js
+++ scripts/hammerGame/game.js
@@ -4,9 +4,9 @@ const mainTextArea = document.querySelector("main");
 
 let hammer = null;
 
-function MoveHammerToCursor(e) {
-    hammer.style.top = `${e.clientY}px`;
-    hammer.style.left = `${e.clientX}px`;
+function MoveHammerToCursor(x, y) {
+    hammer.style.top = `${y}px`;
+    hammer.style.left = `${x}px`;
 }
 
 baseHammerElement.addEventListener("click", e => {
@@ -19,7 +19,7 @@ baseHammerElement.addEventListener("click", e => {
 
     document.body.appendChild(hammer);
 
-    MoveHammerToCursor(e)
+    MoveHammerToCursor(e.clientX, e.clientY)
     // Hide base hammer \\
     baseHammerElement.style.opacity = "0";
     baseHammerElement.style.pointerEvents = "none";
@@ -31,7 +31,7 @@ baseHammerElement.addEventListener("click", e => {
 document.addEventListener("mousemove", e => {
     if (!hammer) return; // Exit if user isn't dragging the hammer
 
-    MoveHammerToCursor(e)
+    MoveHammerToCursor(e.clientX, e.clientY)
 })
 
 
@@ -77,7 +77,7 @@ function GetRandomPos() {
         }
     }
 
-    return {x: spawnPosX, y: spawnPosY};
+    return { x: spawnPosX, y: spawnPosY };
 }
 
 function StartGame() {
@@ -86,11 +86,14 @@ function StartGame() {
 
     mainTextArea.classList.add("no-select");
 
-    const MAXIMUM_MAX_ENEMY_BURST = 5;
+
+    const MAXIMUM_MAX_ENEMY_BURST = 3;
     const LOSE_ENEMY_COUNT = 30;
-    
-    const START_ENEMY_SPAWN_DELAY = 2000;
-    const MIN_ENEMY_SPAWN_DELAY = 600;
+
+    const START_ENEMY_SPAWN_DELAY = 1700;
+    const MIN_ENEMY_SPAWN_DELAY = 1000;
+
+    const ENEMY_SPAWN_DELAY_DECREASE = 80;
 
     const ENEMY_SPAWN_DELAY_RANDOMNESS_RANGE = 300;
 
@@ -103,37 +106,130 @@ function StartGame() {
 
     let enemiesKilled = 0;
 
+    let enemyBurstSpawnIncreaseFactor = 2; // How many kills the player has to get before max enemies increase
+    let enemySpawnIncreaseFactor = 6;
+
+    // Dev Stats \\
+    const killsDevStats = document.getElementById("dev-stats-kills")
+    const burstDevStats = document.getElementById("dev-stats-burst")
+    const burstIncreaseFactorDevStats = document.getElementById("dev-stats-burstIncreaseFactor")
+    const spawnIncreaseFactorDevStats = document.getElementById("dev-stats-spawnIncreaseFactor")
+    const spawnDelayDevStats = document.getElementById("dev-stats-spawnDelay")
+
+    //eslint-disable-next-line
+    function runDevStatsUpdater() {
+        killsDevStats.innerText = `Kills: ${enemiesKilled}`;
+        spawnDelayDevStats.innerText = `Spawn Delay: ${enemySpawnDelay}`;
+
+        burstDevStats.innerText = `Max Burst Count: ${maxEnemyBurstCount}`;
+        spawnIncreaseFactorDevStats.innerText = `Spawn Increase Factor: ${enemySpawnIncreaseFactor}`;
+        burstIncreaseFactorDevStats.innerText = `Burst Increase Factor: ${enemyBurstSpawnIncreaseFactor}`;
+
+        setTimeout(runDevStatsUpdater, 100)
+
+    }
+    //runDevStatsUpdater()
+
     // ### UI ### \\
     const gameUI = document.getElementById("game-ui");
     const enemyUI = document.getElementById("enemy-count");
 
     gameUI.style.display = "block";
 
-    enemyUI.innerText = enemiesCount;
+
+    function UpdateEnemyUI() {
+        enemyUI.innerText = enemiesCount;
+
+        if (enemiesCount >= 20) {
+            // Make red depending on enemy count \\
+            // Normalize value to 0-1
+            const t = (enemiesCount - 20) / (LOSE_ENEMY_COUNT - 20);
+
+            // Define start and end colors
+            const startColor = { r: 255, g: 255, b: 255 }; // White
+            const endColor = { r: 255, g: 0, b: 0 }; // Red
+
+            const r = Math.round(startColor.r + (endColor.r - startColor.r) * t);
+            const g = Math.round(startColor.g + (endColor.g - startColor.g) * t);
+            const b = Math.round(startColor.b + (endColor.b - startColor.b) * t);
+
+            enemyUI.style.color = `rgb(${r},${g},${b})`
+        }
+    }
     // #### Hammer #### \\
     // Use Hammer \\
     function UseHammer(e) {
-                hammer.classList.add("hitting");
+        let mouseX, mouseY;
+
+        if (e instanceof TouchEvent) {
+            mouseX = e.touches[0].clientX;
+            mouseY = e.touches[0].clientY
+        } else {
+            mouseX = e.clientX;
+            mouseY = e.clientY;
+        }
+
+        MoveHammerToCursor(mouseX, mouseY)
+
+        hammer.classList.add("hitting");
 
         // Check if hitting an enemy \\
         for (const [i, enemy] of enemies.entries()) {
-            if (posInRect(enemy.getBoundingClientRect(), e.clientX, e.clientY)) {
-                // Hammer has hit enemy \\
-                enemy.remove();
-                enemies.splice(i, 1);
+            if (posInRect(enemy.getBoundingClientRect(), mouseX, mouseY)) {
+                // ##### Hammer has hit enemy ##### \\
+                const enemyType = enemy.dataset.type;
 
-                enemiesCount--;
-                enemyUI.innerText = enemiesCount;
+                function KillEnemy() {
+                    // Death Animation \\
+                    enemy.classList.add("death")
+                    setTimeout(() => {
+                        enemy.remove()
+                    }, 1000)
 
-                enemiesKilled++;
+                    // Remove elsewhere \\
+                    enemies.splice(i, 1);
 
-                // Increase difficulty \\
-                if (isMultiple(enemiesKilled, 4)) {
-                    if (enemySpawnDelay > MIN_ENEMY_SPAWN_DELAY) enemySpawnDelay -= 100;
+                    enemiesCount--;
+                    UpdateEnemyUI();
+
+                    enemiesKilled++;
                 }
 
-                if (isMultiple(maxEnemyBurstCount, 6)) {
+                // Enemy logic \\
+                switch (enemyType) {
+                    case "brute": {
+                        const lives = Number(enemy.dataset.lives);
+
+                        if (lives <= 1) {
+                            enemy.classList.remove("hit");
+                            KillEnemy()
+                        } else {
+                            enemy.dataset.lives = lives - 1;
+                            enemy.classList.add("hit");
+                            setTimeout(() => {
+                                enemy.classList.remove("hit");
+                            }, 1000)
+                        }
+                        break
+                    }
+                    case "glass": {
+                        EndGame("glass");
+                        break
+                    }
+                    default: {
+                        KillEnemy()
+                    }
+                }
+
+                // Increase difficulty \\
+                if (isMultiple(enemiesKilled, Math.floor(enemySpawnIncreaseFactor))) {
+                    if (enemySpawnDelay > MIN_ENEMY_SPAWN_DELAY) enemySpawnDelay -= ENEMY_SPAWN_DELAY_DECREASE;
+                    enemySpawnIncreaseFactor += 0.5;
+                }
+
+                if (isMultiple(maxEnemyBurstCount, Math.floor(enemyBurstSpawnIncreaseFactor))) {
                     if (maxEnemyBurstCount < MAXIMUM_MAX_ENEMY_BURST) maxEnemyBurstCount++;
+                    enemyBurstSpawnIncreaseFactor += 3;
                 }
             }
         }
@@ -144,13 +240,39 @@ function StartGame() {
     }
 
     document.addEventListener("mousedown", UseHammer)
+    document.addEventListener("touchstart", UseHammer)
+
+    document.addEventListener("touchend", UseHammer)
     document.addEventListener("mouseup", LiftUpHammer)
 
     // ##### Enemy Spawning ####
-    function SpawnEnemy(type) {
+    const enemyTypes = [
+        { type: "basic", chance: 60 },
+        { type: "brute", chance: 30 },
+        { type: "glass", chance: 10 }
+    ]
+
+    function SpawnEnemy(disableLogic, typeOverride) {
         const enemy = document.createElement("div");
 
         enemy.classList.add("enemy");
+        // Get Enemy type
+        let type;
+
+        if (!typeOverride) {
+            const randomPercent = Math.random() * 100;
+            let cumulative = 0;
+
+            for (const enemy of enemyTypes) {
+                cumulative += enemy.chance;
+                if (randomPercent < cumulative) {
+                    type = enemy.type;
+                    break;
+                }
+            }
+
+        } else type = typeOverride;
+
         enemy.classList.add(type);
 
         // Set position \\
@@ -163,24 +285,50 @@ function StartGame() {
 
         enemies.push(enemy);
 
-        enemiesCount++;
-        enemyUI.innerText = enemiesCount;
+        if (type !== "glass") enemiesCount++;
+
+        UpdateEnemyUI()
+
+        // Enemy logic \\
+        if (disableLogic) return; // Exit early if logic is disabled
+
+        enemy.dataset.type = type;
+
+        switch (type) {
+            case "brute": {
+                enemy.dataset.lives = 2;
+                break;
+            }
+            case "glass": {
+                // Remove after 10 seconds \\
+                setTimeout(() => {
+                    enemy.classList.add("death")
+                    setTimeout(() => {
+                        enemy.remove();
+                    }, 1000)
+                }, 10000)
+            }
+        }
     }
 
+    let stopSpawning = false;
+
     function SpawnEnemyTimeout() {
+        if (stopSpawning) return;
+
         if (enemiesCount < LOSE_ENEMY_COUNT - 1) {
-            SpawnEnemy("basic");
+            SpawnEnemy();
 
             // Enemy burst spawning \\
             // Spawn enemies in "bursts", as in a few in quick sucession if allowed
-            if (getRandomInt(1, 3) === 3) {
+            if (getRandomInt(1, 3) === 3 && !stopSpawning) {
                 console.log("burst spawn")
                 for (let i = 1; i < getRandomInt(1, maxEnemyBurstCount); i++) {
                     setTimeout(() => {
-                        if (enemiesCount < LOSE_ENEMY_COUNT - 1) {
-                            SpawnEnemy("basic")
+                        if (enemiesCount < LOSE_ENEMY_COUNT - 1 && !stopSpawning) {
+                            SpawnEnemy()
                         }
-                    }, getRandomInt(800 * i, 1500 * i))
+                    }, 1200 * i)
                 }
             }
             setTimeout(SpawnEnemyTimeout, getRandomInt(
@@ -188,35 +336,63 @@ function StartGame() {
                 enemySpawnDelay
             ));
         } else {
-            // Player has lost! \\
-            console.log("Game over!");
+            EndGame()
+        }
+    }
 
+    SpawnEnemyTimeout()
+
+
+    function EndGame(enemyTypeAnimationOverride) {
+        // Player has lost! \\
+        console.log("Game over!");
+
+        stopSpawning = true;
+
+        // Remove events \\
+        document.removeEventListener("mousedown", UseHammer)
+        document.removeEventListener("touchstart", UseHammer)
+
+        document.removeEventListener("touchend", UseHammer)
+        document.removeEventListener("mouseup", LiftUpHammer)
+
+
+
+        // Spawn a ton of enemies for death animation \\
+        SpawnEnemy(true, enemyTypeAnimationOverride)
+
+        for (let i = 0; i < 300; i++) {
+            setTimeout(() => {
+                SpawnEnemy(true, enemyTypeAnimationOverride)
+            }, (5 * i) + 1000)
+        }
+
+        setTimeout(() => {
             // Revert to start \\
             // Reset hammer \\
             hammer.remove();
             baseHammerElement.style.opacity = "1";
             baseHammerElement.style.pointerEvents = "all";
 
-            // Remove events \\
-            document.removeEventListener("mousedown", UseHammer);
-            document.removeEventListener("mouseup", LiftUpHammer);
 
             // Remove enemies \\
-            Array.from(enemySpawnZone.children).forEach(enemy => {
-                enemy.remove()
-            })
+            const enemiesToRemove = Array.from(enemySpawnZone.children);
+
+            enemiesToRemove.forEach(enemy => enemy.classList.add("death"));
+
+            setTimeout(() => {
+                enemiesToRemove.forEach(enemy => enemy.remove());
+            }, 2000);
 
             // Other \\
             mainTextArea.classList.remove("no-select");
             document.body.style.cursor = "auto";
 
             gameUI.style.display = "none";
-        }
 
+            enemyUI.style.color = "";
+        }, 5 * 300 + 1100)
     }
-
-    SpawnEnemyTimeout()
-
 
 }
 
